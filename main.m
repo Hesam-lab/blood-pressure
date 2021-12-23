3f75f6fea158e12d2009bfeefca732d2e3507967
Hesam-lab <hesamshokouhalaei@gmail.com>
2021-12-23 14:09:52 +0330

    Update main.m

--- main.m
+++ main.m
@@ -1,9 +1,9 @@
 clc; clear; close all;
-% In this code, the aortic blood pressure (sig1) is estimated from radial
-% blood pressure (sig2) by AR model based on two least-mean-square (LMS)
+% In this code, the aortic blood pressure is estimated from radial
+% blood pressure by AR model based on two least-mean-square (LMS)
 % approaches:
 % 1- Offline method: all training samples are inserted into LMS equation,
-% and through several iteration (epoch), the AR coefficient (w) is
+% and through several iteration (epoch), the AR coefficients (w) are
 % calculated.
 % 2- online method: For each given input and output samples, w is computed
 % iteratively. Thus the number of epoch is equal to total samples
@@ -13,37 +13,37 @@ clc; clear; close all;
 % 
 % data description==> Fs:200 Hz, length: 10 seconds
 data = load('blood_pressure.mat');
-sig1 = data.aortic_data;    % model output
-sig2 = data.radial_data;    % model input
+sig1 = data.radial_data;    % model input
+sig2 = data.aortic_data;    % model output
 
-N=length(sig2)*7/10;
-ts_train = sig2(1:N);   % train data: 7 seconds
-ts_test = sig2(N+1:end);    % test data: 3 seconds
+N=length(sig1)*7/10;
+ts_train = sig1(1:N);   % train data: 7 seconds
+ts_test = sig1(N+1:end);    % test data: 3 seconds
 M = length(ts_test);
 
 p = 20; % AR order
-d_train = sig1(1:N-p)';
-x_train = arlag(ts_train,N,p);  % create lag matrix 
+y_train = sig2(1:N-p)';
+x_train = arlag(ts_train,N,p);  % create lag matrix of radial signal
 
 % online least mean square
 mu = 2e-4;  %learning rate
-w = onlinelms(x_train,d_train,mu,p);    % AR parameters
+w = onlinelms(x_train,y_train,mu,p);    % AR parameters
 
 % offline least mean square
 % mu = 2e-7;    %learning rate
 % epoch = 1e4;    % number of iteration
-% w = batchlms(x_train,d_train,mu,p,epoch); % AR parameters
+% w = batchlms(x_train,y_train,mu,p,epoch); % AR parameters
 
-d_test = sig1(N+1:end-p)';
+y_test = sig2(N+1:end-p)';
 x_test = arlag(ts_test,M,p);
 y(:,1) = x_test*w(end,:)';
-rmse = sqrt(mse(d_test - y))    % root mean square error
-cc = corrcoef(d_test,y)     % correlation coefficient
+rmse = sqrt(mse(y_test - y))    % root mean square error
+cc = corrcoef(y_test,y)     % correlation coefficient
 
 % plot 
 figure(1);
 t = linspace(7,10,M-p);
-plot(t,d_test)
+plot(t,y_test)
 hold on
 plot(t,y)
 legend ('real aortic blood pressure','simulated aortic blood pressure')
